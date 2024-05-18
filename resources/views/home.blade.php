4d357ca6717cf15e152111ef7a21e86f60353f54
Joe Gene Quesada <jlquesada@apc.edu.ph>
2024-05-18 16:49:18 +0800

    Laravel Daily Lesson 05

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -109,18 +109,11 @@
                             <div class="row">
                                 <div class="col-sm-6">
                                     <ul class="list-unstyled mb-0">
-                                        <li><a href="#!">Web Design</a></li>
-                                        <li><a href="#!">HTML</a></li>
-                                        <li><a href="#!">Freebies</a></li>
+                                       @foreach($categories as $category) 
+                                        <li><a href="#!">{{ $category }}</a></li>
+                                        @endforeach 
                                     </ul>
-                                </div>
-                                <div class="col-sm-6">
-                                    <ul class="list-unstyled mb-0">
-                                        <li><a href="#!">JavaScript</a></li>
-                                        <li><a href="#!">CSS</a></li>
-                                        <li><a href="#!">Tutorials</a></li>
-                                    </ul>
-                                </div>
+                                </div>                          
                             </div>
                         </div>
                     </div>
