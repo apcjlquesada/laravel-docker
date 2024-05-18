1b95ce97e63e1f69b986f684118e319806834f7f
Joe Gene Quesada <jlquesada@apc.edu.ph>
2024-05-18 17:06:46 +0800

    Lesson 06B Laravel Daily

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Category;
+
 class HomeController extends Controller
 {
     public function index()
     {   
-        $allCategories = ['Category 1', 'Category 2', 'Category 3']; 
+        //$allCategories = ['Category 1', 'Category 2', 'Category 3']; 
+        //$allCategories = DB::table('categories')->get();
+        $allCategories = Category::all();          
 
         return view('home', ['categories' => $allCategories]); 
     }
