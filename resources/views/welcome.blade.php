a25ba91291392eebc25ae8e1790cc00498d4ee40
Joe Gene Quesada <jlquesada@apc.edu.ph>
2024-05-18 14:56:48 +0800

    commit from WSL2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,7 +92,7 @@
                                         <div class="pt-3 sm:pt-5 lg:pt-0">
                                             <h2 class="text-xl font-semibold text-black dark:text-white">Documentation</h2>
                                             <h2 class="text-xl font-semibold text-black dark:text-white">WSL2 commit</h2>
-                                            <h2 class="text-xl font-semibold text-black dark:text-white">Codespaces commit commit</h2>
+                                            <h2 class="text-xl font-semibold text-black dark:text-white">Codespaces commit</h2>
                                             
 
                                             <p class="mt-4 text-sm/relaxed">
