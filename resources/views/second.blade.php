8d6fd45cb4ad4652e73d1eb7709308280763e1c5
Joe Gene Quesada <jlquesada@apc.edu.ph>
2024-05-18 16:06:06 +0800

    Laravel Dily Lesson 4

--- resources/views/second.blade.php
+++ resources/views/second.blade.php
@@ -96,7 +96,7 @@
                                             
 
                                             <p class="mt-4 text-sm/relaxed">
-                                                Laravel framework has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
+                                                Laravel second framework has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
                                             </p>
                                         </div>
                                     </div>
